717acfe7ff2c2eaab8b0cacd5bb9efdf425f2c7e
Martin <martin_n25@yahoo.com>
2021-10-05 12:41:55 +0200

    include latest changes for discount-list

--- resources/views/admin/discount/discount-create.blade.php
+++ resources/views/admin/discount/discount-create.blade.php
@@ -25,41 +25,44 @@ label.lable-custom {
 		<div class="col-md-12 mar-auto">
 		  <div class="back-strip top-side srch-byr">
 				<div class="inner-top">
-				 Create Discocunt 
+				 Create Discocunt
 				</div>
-		  </div>    
+		  </div>
 		</div>
 	    <div class="clearfix"></div>
 		<div class="col-md-12 mar-auto custom-table">
 			<div class="searchtags theme_opt_out">
 						<form method="POST" enctype="multipart/form-data" action="/admin/discountadd" style="margin-bottom: 100px;" autocomplete="off">
-							@csrf						       
-							
+							@csrf
+
 							<div class="ful-top gap-sextion" id="product_container">
 								<div class="col-md-12">
 									<div class="form-group">
 										<label for="end_date" class="lable-custom">Type</label>
 									</div>
 									<div class="form-group">
-										<label class="radio-inline">One Time Uses
-										  <input type="radio" name="type" value="O"  checked>
+										<label class="radio-inline">
+                                            <input type="radio" name="type" value="O"  checked>
+                                            One Time Use
 										</label>
-										<label class="radio-inline">Multiple Uses
-										  <input type="radio" name="type" value="M" >
+										<label class="radio-inline">
+                                            <input type="radio" name="type" value="M" >
+                                            Multiple Uses
 										</label>
-										<label class="radio-inline">World Wide
-										  <input type="radio" name="type" value="W" >
+										<label class="radio-inline">
+                                            <input type="radio" name="type" value="W" >
+                                            Website Wide
 										</label>
-										
+
 									</div>
 									<div class="form-group">
 										<label for="end_date" class="lable-custom">Place </label>
-									</div>	
+									</div>
 									<div class="form-group">
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox placecheckboxes" name="place[]" value="1" >Cart</label>
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox placecheckboxes" name="place[]" value="2" >Price</label>
 									</div>
-									
+
 									<div class="form-group">
 										<label for="end_date" class="lable-custom">Tier </label>
 									</div>
@@ -67,23 +70,23 @@ label.lable-custom {
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox tiercheckbox" name="tier[]" value="1" >Standard Image</label>
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox tiercheckbox" name="tier[]" value="2" >Premium Image</label>
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox tiercheckbox" name="tier[]" value="3" >Deluxe Image</label>
-										
-										<br> 
-										
+
+										<br>
+
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox tiercheckbox" name="tier[]" value="4" >Standard Video</label>
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox tiercheckbox" name="tier[]" value="5" >Premium Video</label>
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox tiercheckbox" name="tier[]" value="6" >Deluxe Video</label>
-										
+
 									</div>
-									
+
 									<div class="form-group">
 										<label for="end_date" class="lable-custom">Content </label>
-									</div>	
+									</div>
 									<div class="form-group">
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox placecheckboxes" name="content[]" value="1" >Image </label>
 										<label class="checkbox-inline"><input type="checkbox" class="custom-checkbox placecheckboxes" name="content[]" value="2" >Video</label>
 									</div>
-									
+
 									<div class="form-group">
 										<label for="coupon" class="lable-custom">Domain <span class="required">*</span></label>
 										<select name="domain_id" class="form-control" required>
@@ -92,7 +95,7 @@ label.lable-custom {
 											@foreach($sitelists as $sitelist)
 												<option value="{{$sitelist->intmanagesiteid}}">{{$sitelist->vchsitename}}</option>
 											@endforeach
-											
+
 										</select>
 									</div>
 									<div class="form-group">
@@ -102,7 +105,7 @@ label.lable-custom {
 											<option value="P">Percentage(%)</option>
 											<option value="A">Price($)</option>
 										</select>
-									</div>	
+									</div>
 									<div class="form-group">
 										<label for="amount" class="lable-custom">Amount <span class="required">*</span></label>
 										<input type="text" name="amount" class="form-control" id="amount">
@@ -115,17 +118,17 @@ label.lable-custom {
 										<label for="end_date" class="lable-custom">Expire  Date</label>
 										<input type="text" name="end_date" class="form-control" id="end_date">
 									</div>
-			
-									
-									
-					
-									
-									
+
+
+
+
+
+
 									<div class="form-group">
 										<label for="number_of_uses" class="lable-custom">Number of Uses</label>
 										<input type="text" name="number_of_uses" class="form-control" id="number_of_uses"  onkeyup="if (/\D/g.test(this.value)) this.value = this.value.replace(/\D/g,'')">
 									</div>
-									
+
 									<div class="form-group">
 									  <label for="note" class="lable-custom">Note:</label>
 									  <textarea class="form-control" name="note" rows="5" id="note"></textarea>
@@ -133,7 +136,7 @@ label.lable-custom {
 
 									<div class="form-group" style="clear:both">
 									  <input type="submit" value="Submit" class="btn btn-dafualt">
-									</div>	
+									</div>
 								</div>
 							</div>
 						</form>
@@ -161,7 +164,7 @@ $( function() {
 		  dateFormat: 'yy-mm-dd'
 	});
   } );
-  
+
 </script>
 
 @include('admin/admin-footer')
--- resources/views/admin/discount/discount-list.blade.php
+++ resources/views/admin/discount/discount-list.blade.php
@@ -14,15 +14,15 @@ a.btn.btn-primary.pull-right {
 }
 </style>
 <div class="row">
-		<div class="col-md-12 mar-auto">
-		  <div class="back-strip top-side srch-byr">
-				<div class="inner-top">
-				 Discocunt List
-				</div>
-		  </div>    
-		</div>
-	    <div class="clearfix"></div>
-		<div class="col-md-12 mar-auto custom-table">
+    <div class="col-md-12 mar-auto">
+      <div class="back-strip top-side srch-byr">
+            <div class="inner-top">
+             Discocunt List
+            </div>
+      </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="col-md-12 mar-auto custom-table">
 			<a href="/admin/creatediscount" class="btn btn-primary pull-right">Add Coupon </a>
 			<div class="table-responsive">
 			  <table class="table">
@@ -42,7 +42,7 @@ a.btn.btn-primary.pull-right {
 				</tr>
 				@foreach($response as $res)
 				  @php
-					  $type = $res->type == 'O' ? 'One Time Users' : ($res->type == 'M' ? 'Multiple Uses' : 'World Wide');
+					  $type = $res->type == 'O' ? 'One Time Use' : ($res->type == 'M' ? 'Multiple Uses' : 'Website Wide');
 					  $place = $res->place == 1 ? 'Cart' : 'Price';
 
                       $tier = '';
@@ -82,27 +82,25 @@ a.btn.btn-primary.pull-right {
 					<td> {{$res->number_of_uses}}</td>
 					<td> {{$res->amount}}</td>
 					<td>
-					@php
-					 $date_now = date("Y-m-d"); // this format is string comparable
-					@endphp 
+                        @php
+                         $date_now = date("Y-m-d"); // this format is string comparable
+                        @endphp
 
-					@if ($date_now < $res->end_date) 
-					
-						@if($res->status == 'A') <span class="alert-success ex-alert" > Active <span>  @elseif($res->status == 'D') <span class=" alert-danger ex-alert" > Inactive <span> @elseif($res->status == 'E') <span class="alert-warning ex-alert" > Expired  <span> @endif 
-					
-					@else
-						<span class="alert-danger ex-alert" > Expired  <span>
-					@endif
+                        @if ($date_now < $res->end_date)
+                            @if($res->status == 'A') <span class="alert-success ex-alert" > Active <span>  @elseif($res->status == 'D') <span class=" alert-danger ex-alert" > Inactive <span> @elseif($res->status == 'E') <span class="alert-warning ex-alert" > Expired  <span> @endif
+                        @else
+                            <span class="alert-danger ex-alert" > Expired  <span>
+                        @endif
 					</td>
-					<td> @if($res->domain_id == 'A') All Domain @else {{$res->vchsitename}} @endif</td>
-					<td> {{$res->note}} </td>
-					<td> @if($res->type == 'E') Everyone @elseif($res->type == 'S') Single @endif  </td>
+					<td>@if($res->domain_id == 'A') All Domains @else {{$res->vchsitename}} @endif</td>
+					<td>{{$res->note}} </td>
+					<td>@if($res->type == 'E') Everyone @elseif($res->type == 'S') Single @endif  </td>
 					<td>
 						<a href="/admin/discountedit/{{$res->id}}" class="btn btn-success  btn-sm" ><i class="fa fa-edit" aria-hidden="true"></i></a>
-						
+
 						<a href="/admin/deletecoupon/{{$res->id}}" class=" btn btn-danger btn-sm"  onclick="return confirm('Are you sure you want to delete this record?')"><i class="fa fa-trash" aria-hidden="true"></i> </a>
 					</td>
-				</tr>	
+				</tr>
 				@endforeach
 			  </table>
 				{{ $response->links() }}
