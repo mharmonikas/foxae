c99ddfeffa6590321024600030db0aeee9dd1573
Martin <martin_n25@yahoo.com>
2021-11-14 19:41:51 +0100

    Display renewed subscription

--- resources/views/purchase_history.blade.php
+++ resources/views/purchase_history.blade.php
@@ -1,5 +1,5 @@
 @include('header')
-<div class="container-fuild container-fuild-maintain">    
+<div class="container-fuild container-fuild-maintain">
 	<div class="row row-fuild-maintain">
 		@include('sidebar')
 		<div class="col-md-8 bg-color">
@@ -15,17 +15,17 @@
 					  <th>Amount</th>
 					  <th>Payment Status</th>
 					  <th>Payment Type</th>
-					 
+
 					  <th>Date</th>
-					 
-					
+
+
 				</tr>
 			</thead>
 						@php
 						$i=1;
-						
+
 						@endphp
-	
+
 						@if(count($response)>0)
 							@foreach($response as $res)
 							  <tr>
@@ -34,17 +34,17 @@
 								<td>@if(!empty($res->plan_download)){{$res->plan_download}} Credits @endif</td>
 								<td>${{ number_format($res->strip_amount, 2) }}</td>
 								<td>@if($res->strip_status='succeeded' || $res->strip_status='active'){{'Successful'}}@elseif($res->strip_status='declined'){{'Failed'}} @else {{'Processing'}} @endif</td>
-								<td>@if($res->strip_package_type=='M')@if($res->strip_payment_type=='New Payment'){{'Subscription'}}@elseif($res->strip_payment_type=='Renew Payment'){{'Subscription-Renew'}}@endif @elseif($res->strip_package_type=='O'){{'One Time Purchase'}}@elseif($res->strip_package_type=='D'){{'Direct Payment'}} @elseif( $res->strip_package_type=='Y')@if($res->strip_payment_type=='New Payment'){{'Subscription'}}@elseif($res->strip_payment_type=='Renew Payment'){{'Subscription-Renew'}}@endif @endif</td>
-							
+								<td>@if($res->strip_package_type=='M')@if($res->strip_payment_type=='New Payment'){{'Subscription'}}@elseif($res->strip_payment_type=='Renew Payment'){{'Subscription - Renew'}}@endif @elseif($res->strip_package_type=='O'){{'One Time Purchase'}}@elseif($res->strip_package_type=='D'){{'Direct Payment'}} @elseif( $res->strip_package_type=='Y')@if($res->strip_payment_type=='New Payment'){{'Subscription'}}@elseif($res->strip_payment_type=='Renew Payment'){{'Subscription-Renew'}}@endif @endif</td>
+
 								<td> {{ \Carbon\Carbon::parse($res->create_date)->format('d-M-Y')}}</td>
-								
+
 							</tr>
 							  @php
 						$i++;
-						
+
 						@endphp
-							@endforeach  
-						@else 
+							@endforeach
+						@else
 							 <tr>
 								<td colspan='5' style="text-align:center"><h2> No Records Found </h2></td>
 							 </tr>
