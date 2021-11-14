b353baa3d1aafa790593b532ed5fedc684d1b80f
Martin <martin_n25@yahoo.com>
2021-11-14 19:40:49 +0100

    Minor changes

--- app/Jobs/UpdateDomainPreviewImagesJob.php
+++ app/Jobs/UpdateDomainPreviewImagesJob.php
@@ -90,7 +90,7 @@ class UpdateDomainPreviewImagesJob implements ShouldQueue
             $backgroundImage->insert($watermarkImage, 'bottom-left');
 
             $backgroundImage->save();
-//
+
 //            $backgroundImage->insert($watermarkImage, 'bottom-left');
 //
 //            $this->assureDirectoryExists('watermarkedImages/'.$this->domainId.'/'.$background->bg_id);
